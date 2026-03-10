d629af0824c64b5c44fb022fe87e35740e7404c7
Huynh Long <longh123123@gmail.com>
2026-03-10 16:30:25 +0700

    vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: '/',
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_BE_URL,
+          changeOrigin: true,
+        },
       },
     },
-  }
-})
+  };
+});
